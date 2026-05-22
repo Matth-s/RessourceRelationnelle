95260057c7abcc40bb5a48f7c931d75f30b25845
Matthieu senosiain <senosiain.matthieu@yaoo.fr>
2026-05-22 11:02:15 +0200

    register scenario front

---
+++ mobile-app/playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/__test__/scenarios',
+
+  use: {
+    baseURL: 'http://localhost:5173',
+    headless: true,
+  },
+
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+  },
+});
